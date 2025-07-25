5eb397227a1ec4d7a04a4f2445df131246cbffc1
L2504902442 <l2504902442as@gmail.com>
2025-07-25 16:46:08 +0900

    課題10完成

---
+++ src/main/java/raisetech/student/management/StudentsCourses.java
@@ -0,0 +1,16 @@
+package raisetech.student.management;
+
+import  lombok.Getter;
+import  lombok.Setter;
+
+@Getter
+@Setter
+public class StudentsCourses {
+    private String courseId;
+    private int studentId;
+    private String courseName;
+    private String startDate;
+    private String endDate;
+
+}
+
