aa825e8534c13f74ee0c087d3acee141a19538df
L2504902442 <l2504902442as@gmail.com>
2025-07-26 14:24:05 +0900

    課題11完成

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -4,32 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.repository.StudentRepository;
 
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-	@Autowired
-	private StudentRepository repository;
-
-	@Autowired
-	private StudentsCoursesRepository studentsCoursesRepository;
-
-
 
 	public static void main(String[] args) {
 	  SpringApplication.run(Application.class, args);
-	}
-
-	@GetMapping("/studentList")
-	public List<Student> getStudentList() {
-		 return repository.search();
-	}
-
-	@GetMapping("/students_coursesList")
-	public List<StudentsCourses> getStudentsCoursesList() {
-		return studentsCoursesRepository.search();
 	}
 }
---
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -0,0 +1,33 @@
+package raisetech.student.management.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+    private StudentRepository repository;
+
+    @Autowired
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Student> searchStudentList() {
+        return repository.search().stream()
+                .filter(student -> student.getAge() >= 30 && student.getAge() < 40 )
+                .collect(Collectors.toList());
+    }
+
+    public List<StudentsCourses> searchStudentsCoursesList() {
+        return repository.searchStudentsCourseList().stream()
+                .filter(courses -> "Java基礎".equals(courses.getCourseName()))
+                .collect(Collectors.toList());
+    }
+}
